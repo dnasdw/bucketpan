096b331e7656ab2093fa188dd95ed6a714d7be67
Sun Daowen <sundaowen@gmail.com>
2020-08-19 09:10:45 +0800

    init bucketpan

---
+++ src/bucketpan/bucketpan.cpp
@@ -0,0 +1,195 @@
+#include "bucketpan.h"
+
+CBucketPan::SOption CBucketPan::s_Option[] =
+{
+	{ nullptr, 0, USTR("action:") },
+	{ USTR("sample"), 0, USTR("show the samples") },
+	{ USTR("help"), USTR('h'), USTR("show this help") },
+	{ nullptr, 0, nullptr }
+};
+
+CBucketPan::CBucketPan()
+	: m_eAction(kActionNone)
+{
+}
+
+CBucketPan::~CBucketPan()
+{
+}
+
+int CBucketPan::ParseOptions(int a_nArgc, UChar* a_pArgv[])
+{
+	if (a_nArgc <= 1)
+	{
+		return 1;
+	}
+	for (int i = 1; i < a_nArgc; i++)
+	{
+		int nArgpc = static_cast<int>(UCslen(a_pArgv[i]));
+		if (nArgpc == 0)
+		{
+			continue;
+		}
+		int nIndex = i;
+		if (a_pArgv[i][0] != USTR('-'))
+		{
+			UPrintf(USTR("ERROR: illegal option\n\n"));
+			return 1;
+		}
+		else if (nArgpc > 1 && a_pArgv[i][1] != USTR('-'))
+		{
+			for (int j = 1; j < nArgpc; j++)
+			{
+				switch (parseOptions(a_pArgv[i][j], nIndex, a_nArgc, a_pArgv))
+				{
+				case kParseOptionReturnSuccess:
+					break;
+				case kParseOptionReturnIllegalOption:
+					UPrintf(USTR("ERROR: illegal option\n\n"));
+					return 1;
+				case kParseOptionReturnNoArgument:
+					UPrintf(USTR("ERROR: no argument\n\n"));
+					return 1;
+				case kParseOptionReturnUnknownArgument:
+					UPrintf(USTR("ERROR: unknown argument \"%") PRIUS USTR("\"\n\n"), m_sMessage.c_str());
+					return 1;
+				case kParseOptionReturnOptionConflict:
+					UPrintf(USTR("ERROR: option conflict\n\n"));
+					return 1;
+				}
+			}
+		}
+		else if (nArgpc > 2 && a_pArgv[i][1] == USTR('-'))
+		{
+			switch (parseOptions(a_pArgv[i] + 2, nIndex, a_nArgc, a_pArgv))
+			{
+			case kParseOptionReturnSuccess:
+				break;
+			case kParseOptionReturnIllegalOption:
+				UPrintf(USTR("ERROR: illegal option\n\n"));
+				return 1;
+			case kParseOptionReturnNoArgument:
+				UPrintf(USTR("ERROR: no argument\n\n"));
+				return 1;
+			case kParseOptionReturnUnknownArgument:
+				UPrintf(USTR("ERROR: unknown argument \"%") PRIUS USTR("\"\n\n"), m_sMessage.c_str());
+				return 1;
+			case kParseOptionReturnOptionConflict:
+				UPrintf(USTR("ERROR: option conflict\n\n"));
+				return 1;
+			}
+		}
+		i = nIndex;
+	}
+	return 0;
+}
+
+int CBucketPan::CheckOptions()
+{
+	if (m_eAction == kActionNone)
+	{
+		UPrintf(USTR("ERROR: nothing to do\n\n"));
+		return 1;
+	}
+	return 0;
+}
+
+int CBucketPan::Help()
+{
+	UPrintf(USTR("bucketpan %") PRIUS USTR(" by dnasdw\n\n"), AToU(BUCKETPAN_VERSION).c_str());
+	UPrintf(USTR("usage: bucketpan [option...] [option]...\n\n"));
+	UPrintf(USTR("option:\n"));
+	SOption* pOption = s_Option;
+	while (pOption->Name != nullptr || pOption->Doc != nullptr)
+	{
+		if (pOption->Name != nullptr)
+		{
+			UPrintf(USTR("  "));
+			if (pOption->Key != 0)
+			{
+				UPrintf(USTR("-%c,"), pOption->Key);
+			}
+			else
+			{
+				UPrintf(USTR("   "));
+			}
+			UPrintf(USTR(" --%-8") PRIUS, pOption->Name);
+			if (UCslen(pOption->Name) >= 8 && pOption->Doc != nullptr)
+			{
+				UPrintf(USTR("\n%16") PRIUS, USTR(""));
+			}
+		}
+		if (pOption->Doc != nullptr)
+		{
+			UPrintf(USTR("%") PRIUS, pOption->Doc);
+		}
+		UPrintf(USTR("\n"));
+		pOption++;
+	}
+	return 0;
+}
+
+int CBucketPan::Action()
+{
+	if (m_eAction == kActionSample)
+	{
+		return sample();
+	}
+	if (m_eAction == kActionHelp)
+	{
+		return Help();
+	}
+	return 0;
+}
+
+CBucketPan::EParseOptionReturn CBucketPan::parseOptions(const UChar* a_pName, int& a_nIndex, int a_nArgc, UChar* a_pArgv[])
+{
+	if (UCscmp(a_pName, USTR("sample")) == 0)
+	{
+		if (m_eAction == kActionNone)
+		{
+			m_eAction = kActionSample;
+		}
+		else if (m_eAction != kActionSample && m_eAction != kActionHelp)
+		{
+			return kParseOptionReturnOptionConflict;
+		}
+	}
+	else if (UCscmp(a_pName, USTR("help")) == 0)
+	{
+		m_eAction = kActionHelp;
+	}
+	return kParseOptionReturnSuccess;
+}
+
+CBucketPan::EParseOptionReturn CBucketPan::parseOptions(int a_nKey, int& a_nIndex, int a_nArgc, UChar* a_pArgv[])
+{
+	for (SOption* pOption = s_Option; pOption->Name != nullptr || pOption->Key != 0 || pOption->Doc != nullptr; pOption++)
+	{
+		if (pOption->Key == a_nKey)
+		{
+			return parseOptions(pOption->Name, a_nIndex, a_nArgc, a_pArgv);
+		}
+	}
+	return kParseOptionReturnIllegalOption;
+}
+
+int CBucketPan::sample()
+{
+	UPrintf(USTR("sample:\n"));
+	return 0;
+}
+
+int UMain(int argc, UChar* argv[])
+{
+	CBucketPan tool;
+	if (tool.ParseOptions(argc, argv) != 0)
+	{
+		return tool.Help();
+	}
+	if (tool.CheckOptions() != 0)
+	{
+		return 1;
+	}
+	return tool.Action();
+}
---
+++ src/bucketpan/bucketpan.h
@@ -0,0 +1,44 @@
+#ifndef BUCKETPAN_H_
+#define BUCKETPAN_H_
+
+#include <sdw.h>
+
+class CBucketPan
+{
+public:
+	enum EParseOptionReturn
+	{
+		kParseOptionReturnSuccess,
+		kParseOptionReturnIllegalOption,
+		kParseOptionReturnNoArgument,
+		kParseOptionReturnUnknownArgument,
+		kParseOptionReturnOptionConflict
+	};
+	enum EAction
+	{
+		kActionNone,
+		kActionSample,
+		kActionHelp
+	};
+	struct SOption
+	{
+		const UChar* Name;
+		int Key;
+		const UChar* Doc;
+	};
+	CBucketPan();
+	~CBucketPan();
+	int ParseOptions(int a_nArgc, UChar* a_pArgv[]);
+	int CheckOptions();
+	int Help();
+	int Action();
+	static SOption s_Option[];
+private:
+	EParseOptionReturn parseOptions(const UChar* a_pName, int& a_nIndex, int a_nArgc, UChar* a_pArgv[]);
+	EParseOptionReturn parseOptions(int a_nKey, int& a_nIndex, int a_nArgc, UChar* a_pArgv[]);
+	int sample();
+	EAction m_eAction;
+	UString m_sMessage;
+};
+
+#endif	// BUCKETPAN_H_
